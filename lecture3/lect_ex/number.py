4714e6f3ad6bd6ee75cf201f0f34145066f952c6
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-02-18 13:56:00 -0600

    Update number.py

--- lecture3/lect_ex/number.py
+++ lecture3/lect_ex/number.py
@@ -1,6 +1,8 @@
 # exception handling
 
-x = int(input("What's x? "))
+try:
+    x = int(input("What's x? "))
 
-print(f"x is {x}")
-
+    print(f"x is {x}")
+except ValueError:
+    print("x is not an integer")
