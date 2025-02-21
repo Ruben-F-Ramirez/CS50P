feca734fa914bf93aa336f4bc00b45854df4b5a8
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-02-21 15:27:15 -0600

    Update say.py

---
+++ lecture4/lec_ex/say.py
@@ -0,0 +1,10 @@
+# using packages
+
+# use pip install in terminal to install packages
+# ex: pip install cowsay
+
+import cowsay
+import sys
+
+if len(sys.argv) == 2:
+    cowsay.cow('Hello World')
