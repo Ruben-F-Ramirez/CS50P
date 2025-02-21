3543edf53669e06d586af77200304bc4276bef06
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-02-21 14:28:05 -0600

    Update name.py
    using sys.exit for errors

--- lecture4/lec_ex/name.py
+++ lecture4/lec_ex/name.py
@@ -3,12 +3,9 @@
 import sys
 
 if len(sys.argv) < 2:
-    print("Too few arguments")
+    sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("Hello, my name is", sys.argv[1])
-""" try:
-    print("Hello, my name is", sys.argv[1])
-except IndexError:
-    print("Too few arguments") """
+    sys.exit("Too many arguments")
+
+print("Hello, my name is", sys.argv[1])
+
