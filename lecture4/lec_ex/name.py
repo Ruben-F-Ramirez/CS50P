f41927644312eb85a310506dcf7abf2d23aab607
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-02-21 14:18:32 -0600

    Update name.py
    alternate indexerror handling

--- lecture4/lec_ex/name.py
+++ lecture4/lec_ex/name.py
@@ -2,7 +2,13 @@
 
 import sys
 
-try:
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("Hello, my name is", sys.argv[1])
+""" try:
     print("Hello, my name is", sys.argv[1])
 except IndexError:
-    print("Too few arguments")
+    print("Too few arguments") """
