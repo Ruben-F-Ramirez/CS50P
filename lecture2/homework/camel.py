aa7075ec65bd504a388f95148a72162e2f517988
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-02-24 13:48:48 -0600

    Update camel.py

--- lecture2/homework/camel.py
+++ lecture2/homework/camel.py
@@ -1,2 +1,17 @@
 # problem set 2 camel case
 
+# get user input
+def main():
+    prompt = snake(input("camel case: "))
+
+# change camel case to snake case
+# ex: firstName -> first_name
+def snake(camel):
+    print("snake case: ", end="")
+    for c in camel:
+        if c.isupper():
+            print(f"_{c.lower()}", end="")
+        else:
+            print(c, end="")
+
+main()
