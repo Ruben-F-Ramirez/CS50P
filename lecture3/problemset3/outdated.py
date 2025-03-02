d0b4e79e1f52297abe673d624c04634acbacdf26
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-02 13:11:52 -0600

    Update outdated.py

--- lecture3/problemset3/outdated.py
+++ lecture3/problemset3/outdated.py
@@ -16,6 +16,13 @@ month = [
 
 
 def main():
-    print(month)
+    date = get_input()
+    convert_date()
+
+def get_input() -> str:
+    return input("Enter date in the format MM/DD/YYYY or September 1, 2025\n")
+
+def convert_date():
+    ...
 
 main()
