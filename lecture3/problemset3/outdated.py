d467229beb19b6049e1460581043c93fc13ac504
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-02 12:40:12 -0600

    Update outdated.py

---
+++ lecture3/problemset3/outdated.py
@@ -0,0 +1,21 @@
+month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+
+
+def main():
+    print(month)
+
+main()
