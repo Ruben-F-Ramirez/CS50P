783ef317ff22a8cae9706e277192e42d9c895535
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-09 15:02:09 -0500

    Update bitcoin.py

--- lecture4/problem_set4/bitcoin.py
+++ lecture4/problem_set4/bitcoin.py
@@ -2,5 +2,28 @@ import json
 import requests
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit("Missing command-line argument")
+
+def main():
+    
+    call_price(com_check())
+
+def call_price(val):
+    try:
+        r = requests.get('https://api.coincap.io/v2/assets/bitcoin')
+        response = r.json()
+        bitcoin = float(response["data"]["priceUsd"])
+        amount = bitcoin * val
+        print(f"${amount:,.4f}")
+    except requests.RequestException:
+        sys.exit("Request exception")
+
+def com_check():
+    if len(sys.argv) != 2:
+        sys.exit("Missing command-line argument")
+    
+    try:
+        return float(sys.argv[1])
+    except:
+        sys.exit("Command-line argument is not a number")
+
+main()
