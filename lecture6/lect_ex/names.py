5919e851c05788d3884deaeea5530ae92b4b5343
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-12 13:30:16 -0500

    update

--- lecture6/lect_ex/names.py
+++ lecture6/lect_ex/names.py
@@ -1,6 +1,7 @@
-names = []
+name = input("What's your name?")
 
-for _ in range(3):
-    names.append(input("What's your name?"))
-for name in sorted(names):
-    print(f"Hello, {name}")
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
+
+
+
