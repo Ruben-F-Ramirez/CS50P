97c1252435b1b02ab8a4036f85dee491d8bd44b6
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-12 12:50:13 -0500

    finished fuel test

--- lecture5/problem_set5/test_fuel/fuel.py
+++ lecture5/problem_set5/test_fuel/fuel.py
@@ -1,13 +1,34 @@
 def main():
-    ...
+    while True:
+        try:
+            percent = convert(input("Fraction: "))
+            break
+        except ValueError:
+            continue
+        except ZeroDivisionError:
+            continue
+        print(gauge(percent))
 
 
-def convert(fraction):
-    ...
+def convert(fraction: str) -> int:
+        num, denom = fraction.split(sep= '/')
+        if not num.isdigit():
+            raise ValueError
+        if not denom.isdigit():
+            raise ValueError
+        if denom == "0":
+            raise ZeroDivisionError
+        if int(num) > int(denom):
+            raise ValueError
+        return round(int(num) / int(denom) * 100)
 
-
-def gauge(percentage):
-    ...
+def gauge(percentage: int) -> str:
+    if percentage <= 1:
+        return "E"
+    elif percentage >= 99:
+        return "F"
+    else:
+        return f"{percentage}%"
 
 
 if __name__ == "__main__":
--- lecture5/problem_set5/test_fuel/test_fuel.py
+++ lecture5/problem_set5/test_fuel/test_fuel.py
@@ -1,2 +1,21 @@
 
 from fuel import convert, gauge
+
+import pytest
+
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        convert("1/0")
+
+def test_float():
+    with pytest.raises(ValueError):
+        convert("1.5/3")
+
+def test_str():
+    with pytest.raises(ValueError):
+        convert("cat/dog")
+
+def test_big_num():
+    with pytest.raises(ValueError):
+        convert("4/3")
