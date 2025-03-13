14b0e2f40918a2aa2214fd94f3558ac0535c0586
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-13 11:40:18 -0500

    update

--- lecture6/lect_ex/students.py
+++ lecture6/lect_ex/students.py
@@ -1,18 +1,13 @@
+import csv
 
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+    reader = csv.reader(file)
+    for name, home in reader:
+        students.append({"name": name, "home": home})
 
-def get_name(student):
-    return student["name"]
-def get_house(student):
-    return student["house"]
-
-print(students[0]["name"])
+#print(students[0]["name"])
 
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is in {student['home']}")
