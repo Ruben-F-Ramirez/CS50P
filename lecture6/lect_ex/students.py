fc9db0b69a5f4b15072fa2b1e979bd87a914b81a
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-13 11:26:03 -0500

    Update students.py

--- lecture6/lect_ex/students.py
+++ lecture6/lect_ex/students.py
@@ -1,5 +1,18 @@
 
+students = []
+
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        print(f"{name} is in {house}")
+        student = {"name": name, "house": house}
+        students.append(student)
+
+def get_name(student):
+    return student["name"]
+def get_house(student):
+    return student["house"]
+
+print(students[0]["name"])
+
+for student in sorted(students, key=lambda student: student["name"]):
+    print(f"{student['name']} is in {student['house']}")
