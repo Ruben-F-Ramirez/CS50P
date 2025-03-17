3d887febfc224453e6b3d3d005f27a022770b37c
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-17 13:18:55 -0500

    Update lines.py

---
+++ lecture6/problem_set6/lines.py
@@ -0,0 +1,32 @@
+import sys
+
+
+
+def main():
+    file_check()
+    line_count = file_read()
+
+
+def file_read():
+    try:
+        lines = 0
+        with open(sys.argv[1]) as file:
+            for line in file:
+                lines += 1
+            return lines
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+
+def file_check():
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif sys.argv[1][-3:] != ".py":
+        sys.exit("Not a Python file")
+
+
+
+if __name__ == "__main__":
+    main()
