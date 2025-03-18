10343b79b6424abf9ff2ad6c5600e3f52484afb5
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-18 13:00:02 -0500

    Update pizza.py

--- lecture6/problem_set6/pizza.py
+++ lecture6/problem_set6/pizza.py
@@ -1,15 +1,33 @@
 import sys
+import csv
+
+from tabulate import tabulate
 
 def main():
     file_check()
+    p_list = read_menu()
+    p_table = table_menu(p_list)
+    print(p_table)
+    
+
+def read_menu():
+    pizzas = []  
+    with open(sys.argv[1]) as file:
+        reader = csv.reader(file)
+        for row in reader:
+            pizzas.append({"pizza": row[0], "small": row[1], "large": row[2]})
+    return pizzas
+
+def table_menu(table):
+    return tabulate(table, headers="firstrow", tablefmt="grid")
 
 def file_check():
     if len(sys.argv) < 2:
         sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 2:
         sys.exit("Too many command-line arguments")
-    elif sys.argv[1][-3:] != ".py":
-        sys.exit("Not a Python file")
+    elif not sys.argv[1].endswith(".csv"):
+        sys.exit("Not a CSV file")
 
 
 
