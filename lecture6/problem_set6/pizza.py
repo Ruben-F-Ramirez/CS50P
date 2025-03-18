c99c87400ee2470e980b196225d7c1fc4e1f5161
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-18 12:11:03 -0500

    Create pizza.py

---
+++ lecture6/problem_set6/pizza.py
@@ -0,0 +1,17 @@
+import sys
+
+def main():
+    file_check()
+
+def file_check():
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif sys.argv[1][-3:] != ".py":
+        sys.exit("Not a Python file")
+
+
+
+if __name__ == "__main__":
+    main()
