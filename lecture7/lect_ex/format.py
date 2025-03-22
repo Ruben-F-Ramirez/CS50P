fabc2506eb1b31edac3068ff0531ee421799197d
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-21 19:21:57 -0500

    Update format.py

--- lecture7/lect_ex/format.py
+++ lecture7/lect_ex/format.py
@@ -1,9 +1,10 @@
-
+import re
 
 name = input("What's your name").strip()
 
-if "," in name:
-    last, first = name.split(",")
-    name = f"{first.strip()} {last.strip()}"
+matches = re.search(r"^(.+), ?(.+)$", name)
+
+if matches:
+    name = matches.group(2) + " " + matches.group(1)
 
 print(f"Hello, {name}")
