ec83721c60ca2a0a8be4aa029bfc1d9f6c735a48
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-23 13:21:12 -0500

    Update twitter.py

--- lecture7/lect_ex/twitter.py
+++ lecture7/lect_ex/twitter.py
@@ -1,7 +1,7 @@
-
+import re
 
 url = input("URL: ").strip()
 
-username = url.removeprefix("https://twitter.com/")
+username = re.sub(r"https://twitter.com/","",url)
 
 print(f"username is {username}")
