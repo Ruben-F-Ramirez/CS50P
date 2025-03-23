63bfc9d0b50a4b305d73b60e4996165aa7a0890a
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-23 12:49:50 -0500

    Update format.py

--- lecture7/lect_ex/format.py
+++ lecture7/lect_ex/format.py
@@ -2,9 +2,8 @@ import re
 
 name = input("What's your name").strip()
 
-matches = re.search(r"^(.+), *(.+)$", name)
-
-if matches:
+# :=, is called a walrus operator
+if matches:= re.search(r"^(.+), *(.+)$", name):
     name = matches.group(2) + " " + matches.group(1)
 
 print(f"Hello, {name}")
