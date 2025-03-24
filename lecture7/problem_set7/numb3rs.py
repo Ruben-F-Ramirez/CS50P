0c5771bc79e4742d1a02a52d8450d7a07726a318
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-24 14:33:31 -0500

    update

--- lecture7/problem_set7/numb3rs.py
+++ lecture7/problem_set7/numb3rs.py
@@ -4,13 +4,16 @@ import sys
 
 def main():
     print(validate(input("IPv4 Address: ")))
-
+    
 
 def validate(ip):
-    ...
-
-
-...
+    try:    
+        if re.search(r"^(([1-9]?[0-9]\.)|(1[0-9]{2}\.)|(2[0-4][0-9]\.)|(25[0-5]\.)){3}((([1-9]?[0-9])|(1[0-9]{2})|(2[0-4][0-9])|(25[0-5])){1})$",ip,re.IGNORECASE):
+             return True
+        else:
+             raise ValueError
+    except ValueError:
+        return False
 
 
 if __name__ == "__main__":
--- lecture7/problem_set7/test_numb3rs.py
+++ lecture7/problem_set7/test_numb3rs.py
@@ -1,1 +1,7 @@
+
+from numb3rs import validate
 import pytest
+
+def test_int():
+    with pytest.raises(ValueError):
+        validate("cat")
