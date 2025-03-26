3a6fc6c2d49a13809fcb73d59c61f341ba687f7c
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-26 11:57:42 -0500

    update

--- lecture7/problem_set7/test_working.py
+++ lecture7/problem_set7/test_working.py
@@ -3,5 +3,19 @@ import pytest
 from working import convert
 
 
-def test_int():
+def test_convert_time():
+    assert convert("9 AM to 5 PM") == "0900 to 1700"
     ...
+
+def test_valerror():
+    with pytest.raises(ValueError):
+        convert("09:00 AM - 17:00 PM")
+
+    with pytest.raises(ValueError):
+        convert("9 AM - 5 PM")
+
+    with pytest.raises(ValueError):
+        convert("9:60 AM to 5:60 PM")
+
+    with pytest.raises(ValueError):
+        convert("")
--- lecture7/problem_set7/working.py
+++ lecture7/problem_set7/working.py
@@ -6,8 +6,50 @@ def main():
     print(convert(input("Hours: ")))
 
 
-def convert(s):
-    ...
+def convert(s: str) -> str:
+    """
+    expected input:
+    9:00 AM to 5:00 PM
+    9 AM to 5 PM
+    9:00 AM to 5 PM
+    9 AM to 5:00 PM
+
+    converts input time format to 24 hour time format
+    example:
+    >>> convert(9:00 AM to 5:00 PM)
+    0900 to 1700
+    """
+    time = re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) to (\d{1,2}):?(\d{2})? (AM|PM)$",s)
+    if time:
+        pass
+    else:
+        raise ValueError
+    if time.group(3) == "AM":
+        meridian = 0
+    else:
+        meridian = 12
+
+    # convert first time
+    hour1 = int(time.group(1)) + meridian
+    if time.group(2) == None:
+        minutes1 = 0
+    else:
+        minutes1 = int(time.group(2))
+
+    # convert second time
+    if time.group(6) == "AM":
+        meridian = 0
+    else:
+        meridian = 12
+
+    hour2 = int(time.group(4)) + meridian
+    if time.group(5) == None:
+        minutes2 = 0
+    else:
+        minutes2 = int(time.group(5))
+
+    
+    return f"{hour1:02}{minutes1:02} to {hour2:02}{minutes2:02}"
 
 
 ...
