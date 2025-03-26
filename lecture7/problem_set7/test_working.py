6e311bd2286b3ccc2d8360c1d2aaa249bb9c7e20
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-26 12:24:31 -0500

    update

--- lecture7/problem_set7/test_working.py
+++ lecture7/problem_set7/test_working.py
@@ -5,7 +5,11 @@ from working import convert
 
 def test_convert_time():
     assert convert("9 AM to 5 PM") == "0900 to 1700"
-    ...
+    assert convert("9:00 AM to 5 PM") == "0900 to 1700"
+    assert convert("9:00 AM to 5:00 PM") == "0900 to 1700"
+    assert convert("9 AM to 5:00 PM") == "0900 to 1700"
+    assert convert("9:50 AM to 5 PM") == "0950 to 1700"
+    assert convert("9 AM to 5:23 PM") == "0900 to 1723"
 
 def test_valerror():
     with pytest.raises(ValueError):
@@ -15,7 +19,9 @@ def test_valerror():
         convert("9 AM - 5 PM")
 
     with pytest.raises(ValueError):
-        convert("9:60 AM to 5:60 PM")
+        convert("9:60 AM to 5 PM")
 
     with pytest.raises(ValueError):
-        convert("")
+        convert("9:00 AM to 5:80 PM")
+
+    
--- lecture7/problem_set7/working.py
+++ lecture7/problem_set7/working.py
@@ -1,5 +1,4 @@
 import re
-import sys
 
 
 def main():
@@ -20,21 +19,30 @@ def convert(s: str) -> str:
     0900 to 1700
     """
     time = re.search(r"^(\d{1,2}):?(\d{2})? (AM|PM) to (\d{1,2}):?(\d{2})? (AM|PM)$",s)
-    if time:
-        pass
-    else:
+    if not time:
         raise ValueError
+
+    # convert first time
     if time.group(3) == "AM":
         meridian = 0
     else:
         meridian = 12
 
-    # convert first time
-    hour1 = int(time.group(1)) + meridian
+    if (time.group(1) == 12) and (meridian == 0):
+        hour1 = 0
+    elif (time.group(1) == 12) and (meridian == 12):
+        hour1 = 12
+    else:
+        hour1 = int(time.group(1)) + meridian
+
     if time.group(2) == None:
         minutes1 = 0
     else:
         minutes1 = int(time.group(2))
+
+    # check for valid minutes
+    if not (0 <= minutes1 <= 59):
+        raise ValueError
 
     # convert second time
     if time.group(6) == "AM":
@@ -42,14 +50,26 @@ def convert(s: str) -> str:
     else:
         meridian = 12
 
-    hour2 = int(time.group(4)) + meridian
+    # convert hour
+    if (time.group(4) == 12) and (meridian == 0):
+        hour1 = 0
+    elif (time.group(4) == 12) and (meridian == 12):
+        hour1 = 12
+    else:
+        hour1 = int(time.group(4)) + meridian
+
+    # convert minutes
     if time.group(5) == None:
         minutes2 = 0
     else:
         minutes2 = int(time.group(5))
 
-    
-    return f"{hour1:02}{minutes1:02} to {hour2:02}{minutes2:02}"
+    # check for valid minutes
+    if not (0 <= minutes2 <= 59):
+        raise ValueError
+
+
+    return f"{hour1:02}:{minutes1:02} to {hour2:02}:{minutes2:02}"
 
 
 ...
