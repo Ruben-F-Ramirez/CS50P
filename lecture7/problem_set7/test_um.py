c6168facdb08e24ba18eeff2d160807845a6a0ed
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-28 13:33:55 -0500

    .

--- lecture7/problem_set7/test_um.py
+++ lecture7/problem_set7/test_um.py
@@ -1,1 +1,14 @@
 import pytest
+from um import count
+
+def main():
+    ...
+
+def test_values():
+    assert count("um") == 1
+    assert count("Um?") == 1
+    assert count("Um, thanks for the album.") == 1
+    assert count("Um, thanks, um...") == 2
+
+if __name__ == "__main__":
+    main()
--- lecture7/problem_set7/um.py
+++ lecture7/problem_set7/um.py
@@ -7,10 +7,9 @@ def main():
 
 
 def count(s):
-    ...
+    matches = re.findall(r"(\bum\b)",s,re.IGNORECASE)
+    return len(matches)
 
-
-...
 
 
 if __name__ == "__main__":
