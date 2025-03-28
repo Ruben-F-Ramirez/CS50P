a25c682845b4693c4ea29b93103cb43cf118c2e5
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-28 14:16:33 -0500

    Update student.py

--- lecture8/lect_ex/student.py
+++ lecture8/lect_ex/student.py
@@ -1,15 +1,12 @@
 
 def main():
-    name = get_name()
-    house = get_house()
+    student = get_student()
+    print(f"{student(0)} from {student(1)}")
 
-    print(f"{name} from {house}")
-
-def get_name():
-    return input("Name: ")
-
-def get_house():
-    return input("House: ")
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return (name, house)
 
 if __name__ == "__main__":
     main()
