35cb5b52e6b52b96569438266d268e320b40083a
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-03-31 12:49:18 -0500

    Update student.py

--- lecture8/lect_ex/student.py
+++ lecture8/lect_ex/student.py
@@ -1,15 +1,18 @@
+class Student:
+    ...
+
+
+
 
 def main():
     student = get_student()
-    if student['name'] == "Padmae":
-        student['house'] = "Ravenclaw"
-    
-    print(f"{student['name']} from {student['house']}")
+    print(f"{student.name} from {student.house}")
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return {'house': house, 'name': name}
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
 
 if __name__ == "__main__":
     main()
