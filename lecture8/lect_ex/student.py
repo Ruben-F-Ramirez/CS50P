915313f5ffca630ebb0c6aad8e93129a23252e34
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-04-01 15:06:02 -0500

    Update student.py

--- lecture8/lect_ex/student.py
+++ lecture8/lect_ex/student.py
@@ -2,14 +2,19 @@ class Student:
     def __init__(self, name, house):
         if not name:
             raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"]:
+            raise ValueError
         self.name = name
         self.house = house
+
+        def __str__(self):
+            return "A student"
 
 
 
 def main():
     student = get_student()
-    print(f"{student.name} from {student.house}")
+    print(student)
 
 def get_student():
     name = input("Name: ")
