1508dcfad40aea192dfd5e4f7c851f971349d11f
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-04-06 15:29:06 -0500

    Update seasons.py

--- lecture8/problem_set8/seasons.py
+++ lecture8/problem_set8/seasons.py
@@ -3,24 +3,38 @@
 
 import inflect
 p = inflect.engine()
+import sys
 
 from datetime import date
 
+class Seasons:
+    def __init__(self, dob):
+        self.dob = dob
+        self.today_date = date.today()
+
+    def life_minutes(self, count) -> str:
+        return p.number_to_words(count, andword="") + " " + p.plural_noun('minute', count)
+
+    
+    @staticmethod
+    def get_dob() -> str:
+        try:
+            dob =  input("Date of Birth: ")
+            return Seasons(date.fromisoformat(dob))
+        except ValueError:
+            sys.exit("Invalid date")
+
+    def convert_date(self):
+        days_lived = (self.today_date - self.dob).days
+        return days_lived*24*60
+
+
 
 def main():
-    delta = convert_date(get_birthday())
-    print(life_minutes(delta))
-
-def life_minutes(count) -> str:
-    return p.number_to_words(count, andword="")
-
-def convert_date(date1) -> int:
-    today = date.today.minutes
-    delta = today - date1.minutes()
-    return delta
-
-def get_birthday() -> str:
-    return input("Date of Birth: ")
+    userDob = Seasons.get_dob()
+    minutes = userDob.convert_date()
+    result = userDob.life_minutes(minutes)
+    print(result)
 
 
 if __name__ == "__main__":
