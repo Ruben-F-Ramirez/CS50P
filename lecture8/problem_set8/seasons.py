4632962ab8f9bbfad78877ee88be94520c5021d2
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-04-06 14:49:16 -0500

    Update seasons.py

--- lecture8/problem_set8/seasons.py
+++ lecture8/problem_set8/seasons.py
@@ -1,11 +1,26 @@
+# pip install inflect
+
+
+import inflect
+p = inflect.engine()
+
 from datetime import date
 
 
 def main():
-    ...
+    delta = convert_date(get_birthday())
+    print(life_minutes(delta))
 
+def life_minutes(count) -> str:
+    return p.number_to_words(count, andword="")
 
-...
+def convert_date(date1) -> int:
+    today = date.today.minutes
+    delta = today - date1.minutes()
+    return delta
+
+def get_birthday() -> str:
+    return input("Date of Birth: ")
 
 
 if __name__ == "__main__":
