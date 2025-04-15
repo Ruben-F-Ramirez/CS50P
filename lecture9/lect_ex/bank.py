d6b4e4327ad10695b08a4f8ecf586ec6376fb8cf
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-04-15 13:25:59 -0500

    Update bank.py

--- lecture9/lect_ex/bank.py
+++ lecture9/lect_ex/bank.py
@@ -3,6 +3,17 @@ balance = 0
 
 def main():
     print(f"Balance: {balance}")
+    deposit(100)
+    withdraw(50)
+    print(f"Balance: {balance}")
+
+def deposit(n):
+    global balance
+    balance += n
+
+def withdraw(n):
+    global balance
+    balance -= n
 
 if __name__ == "__main__":
     main()
