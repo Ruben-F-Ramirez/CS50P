5c0618907c6a2b60f2d50fad89ce1b08dae5d976
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-04-15 13:33:04 -0500

    Update bank.py

--- lecture9/lect_ex/bank.py
+++ lecture9/lect_ex/bank.py
@@ -1,19 +1,26 @@
 
-balance = 0
+class Account:
+    def __init__(self):
+        self.balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+    
+    def deposit(self, n):
+        self._balance += n
+
+    def withdraw(self, n):
+        self._balance -= n
+
 
 def main():
-    print(f"Balance: {balance}")
-    deposit(100)
-    withdraw(50)
-    print(f"Balance: {balance}")
+    account = Account()
 
-def deposit(n):
-    global balance
-    balance += n
-
-def withdraw(n):
-    global balance
-    balance -= n
+    print("Balance: ", account.balance)
+    account.deposit(100)
+    account.withdraw(50)
+    print("Balance: ", account.balance)
 
 if __name__ == "__main__":
     main()
