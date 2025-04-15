dd89bcf6fd03c8168a2d27e8ff5586ae3b70849d
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-04-15 13:46:34 -0500

    Update meow.py

--- lecture9/lect_ex/meow.py
+++ lecture9/lect_ex/meow.py
@@ -1,4 +1,18 @@
 
 class Cat:
+    MEOWS = 3
+
     def __init__(self):
         pass
+
+    def meow(self):
+        for _ in range(Cat.MEOWS):
+            print("meow")
+
+def main():
+    cat = Cat()
+    cat.meow()
+
+
+if __name__ == "__main__":
+    main()
