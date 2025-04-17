d21190330c1c49e7a822684876610b70116e1d59
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-04-17 13:13:48 -0500

    Update meow2.py

--- lecture9/lect_ex/meow2.py
+++ lecture9/lect_ex/meow2.py
@@ -1,5 +1,14 @@
 
 def meow(n: int) -> str:
+    """
+    Meow n times
+
+    :param n: number of times to print meow
+    :type n: int
+    :raise TypeError if n is not an int
+    :return: return string of n lines of meow
+    :rtype: str
+    """
     return "meow\n" * n
 
 number: int = int(input("Number: "))
