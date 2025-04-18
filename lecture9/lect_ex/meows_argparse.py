7c4ab038fe274b7cc26450a9af7e07c73b74defd
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-04-18 12:18:01 -0500

    Update meows_argparse.py

--- lecture9/lect_ex/meows_argparse.py
+++ lecture9/lect_ex/meows_argparse.py
@@ -6,7 +6,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description="meow like a cat")
 
-parser.add_argument(-n,help="number of meows")
+parser.add_argument("-n",default=1,type= int,help="number of meows")
 
 args = parser.parse_args()
-
+meows = "meow\n" * args.n
+print(meows,end="")
