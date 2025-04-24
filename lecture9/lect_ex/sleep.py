eb7737e507f6fbaec5698bcd9e116dac3207bcb7
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-04-24 13:06:13 -0500

    Update sleep.py

--- lecture9/lect_ex/sleep.py
+++ lecture9/lect_ex/sleep.py
@@ -2,8 +2,14 @@
 
 def main():
     n = int(input("What's n? "))
+    for s in sheep:
+        print(s)
+
+def sheep(n):
+    flock = []
     for i in range(n):
-        print("🐑" * i)
+        flock.append("🐑" * i)
+    return flock
 
 if __name__ == "__main__":
     main()
